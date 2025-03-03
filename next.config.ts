472e9f78785c5bf720282f040994c17b496143f6
S SIDHARTH <sidharthpunalur@gmail.com>
2025-03-03 07:08:09 +0530

    deploy fix test 2

--- next.config.ts
+++ next.config.ts
@@ -6,23 +6,6 @@ const nextConfig: NextConfig = {
     domains: ["mulearn-backend-test.s3.amazonaws.com"], // Add your image domains here
   },
   trailingSlash: false, // Ensure URLs don't have a trailing slash (important for Vercel)
-  async redirects() {
-    return [
-      {
-        source: "/old-route",
-        destination: "/new-route",
-        permanent: true, // Permanent redirect (301)
-      },
-    ];
-  },
-  async rewrites() {
-    return [
-      {
-        source: "/custom-api",
-        destination: "/api/hello", // Rewrite API calls
-      },
-    ];
-  },
 };
 
 export default nextConfig;

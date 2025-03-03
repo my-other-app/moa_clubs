69f8a19d577a3a6167cc1f8baafc1ab5523c02de
S SIDHARTH <sidharthpunalur@gmail.com>
2025-03-03 08:36:07 +0530

    deploy test 4

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,70 +1,21 @@
-'use client';
-
-import { useState, useEffect } from 'react';
-import { useNavigate } from '@/utils/navigation';
-import { accessToken } from '@/api/accessTocken';
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Welcome from './register/welcome';
 
 
-export default function Home() {
-  const { navigateTo } = useNavigate();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  // Load saved values from sessionStorage when the component mounts
-  useEffect(() => {
-    const savedEmail = sessionStorage.getItem('email');
-    const savedPassword = sessionStorage.getItem('password');
-    if (savedEmail) setEmail(savedEmail);
-    if (savedPassword) setPassword(savedPassword);
-  }, []);
-
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-
-    // Save to sessionStorage
-    sessionStorage.setItem('email', email);
-    sessionStorage.setItem('password', password);
-    accessToken({ username: email, password });
-    // Navigate to the next page
-    navigateTo('/register/uploadImage');
-  };
-
+const Home: NextPage = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="bg-white p-10 rounded-lg shadow-lg w-auto border-gray-700 border-2 border-solid">
-        <h1 className="text-5xl text-black font-bold text-center mb-2 bebas">
-          WELCOME TO MYOTHERAPP
-        </h1>
-        <p className="text-gray-700 text-center mb-8">
-          We provide you with the end-to-end solution for managing events
-        </p>
-        <form onSubmit={handleSubmit}>
-          <label className="block mb-2 font-medium text-gray-900">Username</label>
-          <input
-            type="text"
-            placeholder="Enter Your username"
-            className="w-full p-2 border rounded-lg mb-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-          <label className="block mb-2 font-medium text-gray-900">Password</label>
-          <input
-            type="password"
-            placeholder="Enter Password"
-            className="w-full p-2 border rounded-lg mb-6 focus:outline-none focus:ring-2 focus:ring-blue-500"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-          <button
-            type="submit"
-            className="w-full bg-gray-700 h-15 text-2xl bebas text-white p-2 rounded-lg font-semibold hover:bg-gray-800"
-          >
-            START THE JOURNEY
-          </button>
-        </form>
-      </div>
-    </div>
+    <>
+      <Head>
+        <title>MyOtherApp</title>
+        <meta name="description" content="My Other App Description" />
+      </Head>
+      <main>
+    
+        <Welcome/>
+      </main>
+    </>
   );
-}
+};
+
+export default Home;

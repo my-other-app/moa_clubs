3bbd89077327ccc8c634f35ef83b32f0f744f976
S SIDHARTH <sidharthpunalur@gmail.com>
2025-03-03 08:42:04 +0530

    deploy test 6

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import Welcome from './register/welcome';
+import Welcome from '../components/welcome';
 
 
 const Home: NextPage = () => {

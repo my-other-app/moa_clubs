43a63b055a0448fee8cd7d81c13fe71b550910bd
S SIDHARTH <sidharthpunalur@gmail.com>
2025-03-05 10:18:21 +0530

    deploy bug fix test

--- src/app/dashboard/eventEachEdit/editCreateEvent/page.tsx
+++ src/app/dashboard/eventEachEdit/editCreateEvent/page.tsx
@@ -112,7 +112,6 @@ export default function CreateEvent() {
   };
 
   // Define eventIntrestIds variable
-  const eventIntrestIds: number[] = [];
 
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();

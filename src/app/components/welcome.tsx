9ed7cf0a37078f8a754942346060225ea7fe5052
S SIDHARTH <sidharthpunalur@gmail.com>
2025-03-09 09:29:07 +0530

    wireframe-3 completed

--- src/app/components/welcome.tsx
+++ src/app/components/welcome.tsx
@@ -99,35 +99,35 @@ export default function Welcome() {
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="bg-white p-10 rounded-lg shadow-lg w-auto border-gray-700 border-2 border-solid">
+      <div className="bg-white p-10 rounded-xl w-auto border-gray-700 border border-solid">
         <h1 className="text-5xl text-black font-bold text-center mb-2 bebas">
           WELCOME TO MYOTHERAPP
         </h1>
         <p className="text-gray-700 text-center mb-8">
-          We provide you with an end-to-end solution for managing events
+          We provide you with an end-to-end<br></br> solution for managing events
         </p>
         <form onSubmit={handleSubmit}>
-          <label className="block mb-2 font-medium text-gray-900">Username</label>
+          <label className="block mb-2 font-light text-gray-700">Username</label>
           <input
             type="text"
             placeholder="Enter Your username"
-            className="w-full p-2 border rounded-lg mb-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="w-full p-2 border rounded-lg mb-4 "
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             required
           />
-          <label className="block mb-2 font-medium text-gray-900">Password</label>
+          <label className="block mb-2 font-light text-gray-700">Password</label>
           <input
             type="password"
             placeholder="Enter Password"
-            className="w-full p-2 border rounded-lg mb-6 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="w-full p-2 border rounded-lg mb-6"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             required
           />
           <button
             type="submit"
-            className="w-full bg-gray-700 h-15 text-2xl bebas text-white p-2 rounded-lg font-semibold hover:bg-gray-800"
+            className="w-full bg-[#2C333D] h-15 text-2xl bebas text-white p-2 rounded-lg font-semibold hover:bg-gray-800"
           >
             START THE JOURNEY
           </button>
